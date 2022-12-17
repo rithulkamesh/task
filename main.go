b196f2625327af69f64398d28faadb154d1a577e
Rithul Kamesh <hi@rithul.dev>
2022-12-17 23:18:43 +0530

    Updated Features

--- main.go
+++ main.go
@@ -1,8 +1,34 @@
 package main
 
-import "fmt"
+import (
+	//	"database/sql"
+	//	"github.com/mattn/go-sqlite3"
+	"github.com/urfave/cli"
+	"os"
+	"path/filepath"
+)
+
+var app = cli.NewApp()
 
 func main() {
-	fmt.Println("Hello, World!")
+	task_init()
+}
 
+func task_init() {
+	/*
+
+		Initialise the task program, this handles the creation of the tasks folder under config,
+		Checking if the database exists or not
+		Creating the database if it does not exist
+
+	*/
+
+	var homedir, _ = os.UserHomeDir()
+	path := filepath.Join(homedir, ".config", "task")
+	os.MkdirAll(path, os.ModePerm)
+
+	return
+}
+
+func create_task() {
 }
